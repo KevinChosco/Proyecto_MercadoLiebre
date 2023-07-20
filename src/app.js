c8ee947489683839aa4dab9a93fc247298d814f0
Kchosco20 <kevinchosco20@gmail.com>
2023-07-19 23:43:07 -0300

    Agregar footer
    Agregar el pie de pagina, divididas en secciones con etiquitas h2 y p

--- src/app.js
+++ src/app.js
@@ -2,8 +2,10 @@ const express=require("express");
 const app=express();
 const path=require("path");
 
+const publicPath=path.resolve(__dirname,"../public");
+app.use(express.static(publicPath));
+
 app.listen(8080,()=>console.log("Servidor escuchando en el puerto 8080"));
-app.use(express.static("public"));
 
 app.get("/",function(req,res){
     res.sendFile(path.join(__dirname,"./view/home.html"));
