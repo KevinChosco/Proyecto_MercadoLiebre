87c137381f9631bf0f502e840633633a537adc91
Kchosco20 <kevinchosco20@gmail.com>
2023-08-10 00:22:49 -0300

    Deploy Render

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 const express=require("express");
 const app=express();
 const path=require("path");
+const port=process.env.PORT || 3001;
 
 const publicPath=path.resolve(__dirname,"../public");
 app.use(express.static(publicPath));
 
-app.listen(8080,()=>console.log("Servidor escuchando en el puerto 8080"));
+app.listen(port,()=>console.log("Servidor escuchando en el puerto 8080"));
 
 app.get("/",function(req,res){
     res.sendFile(path.join(__dirname,"./view/home.html"));
